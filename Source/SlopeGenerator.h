066e2cf0345b37a2e3633ea7fa381318c2d66a1f
mars <martin@rebel-it.com>
2017-06-12 17:41:10 +0100

    adding files for Gonies SlopeGenerator firmware

---
+++ Source/SlopeGenerator.h
@@ -0,0 +1,14 @@
+#ifndef __SLOPEGENERATOR_H__
+#define __SLOPEGENERATOR_H__
+
+#include <inttypes.h>
+
+#ifdef __cplusplus
+ extern "C" {
+#endif
+   
+#ifdef __cplusplus
+}
+#endif
+
+#endif // __SLOPEGENERATOR_H__
